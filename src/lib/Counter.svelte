8e5cc5efc52b6271d07199b151cc28688f0372a9
jicking <jicking.dev@gmail.com>
2023-02-27 18:41:29 +0800

    Cleanup

---
+++ src/lib/Counter.svelte
@@ -0,0 +1,21 @@
+<script>
+	let count = 0;
+	function incrementCount(){
+		count += 1;
+	}
+</script>
+
+<div class="counter">
+	<button on:click={incrementCount} class="counter-btn">
+		Count: {count}
+	</button>
+</div>
+
+<style>
+	.counter {
+		margin: 8px 0;
+	}
+	.counter-btn {
+		padding: 8px;
+	}
+</style>
---
+++ src/lib/Header.svelte
@@ -0,0 +1,21 @@
+<script>
+	import { page } from '$app/stores';
+	import logo from '$lib/images/svelte-logo.svg';
+	import github from '$lib/images/github.svg';
+</script>
+
+<header>
+	<div class="brand">LOGO HERE</div>
+	<nav>
+		<ul>
+			<li>
+				<a href="/">Home</a>
+			</li>
+			<li>
+				<a href="/about">About</a>
+			</li>
+		</ul>
+	</nav>
+</header>
+
+<style></style>
