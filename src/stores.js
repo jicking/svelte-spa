c58112e0bb545cab80793116979ed31acd10a398
jicking <jicking.dev@gmail.com>
2023-02-28 11:37:34 +0800

    cleanup

--- src/stores.js
+++ src/stores.js
@@ -1,8 +1,10 @@
 import { writable } from 'svelte/store';
 
+// TODO: move to utils when needed
+const getNumberFromStorage = (key) => Number(localStorage.getItem('count') || 0);
 
 // define store, will get initial val from local if it exists
-export const count = writable(Number(localStorage.getItem('count')) || 0);
+export const count = writable(getNumberFromStorage('count'));
 
 // subscribe to save val to local storage
 count.subscribe((val) => localStorage.setItem('count', val));
